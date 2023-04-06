8f31955852cdf20308ed9b9394db4f765e98fdf9
Zhenyu Dong <315300270@qq.com>
2023-04-06 23:18:04 +0800

    Create removeItem.cpp

---
+++ Array/27.移除元素/removeItem.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        while (i < nums.size()) {
+            if (nums[i] == val) {
+                nums[i] = nums[nums.size() - 1];
+                nums.pop_back();
+            } else {
+                i++;
+            }
+        }
+        return nums.size();
+    }
+};
