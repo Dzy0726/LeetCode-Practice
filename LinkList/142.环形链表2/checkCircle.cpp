3b346d0020b031a5bdf4bda7e4cce4d3cf27d36e
Zhenyu Dong <315300270@qq.com>
2023-05-01 22:26:25 +0800

    Update 双链表法

--- LinkList/142.环形链表2/checkCircle.cpp
+++ LinkList/142.环形链表2/checkCircle.cpp
@@ -24,3 +24,25 @@ public:
         return cur;
     }
 };
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                ListNode *index1 = head;
+                ListNode *index2 = fast;
+                while(index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index1;
+            }
+        }
+        return NULL;
+    }
+};
