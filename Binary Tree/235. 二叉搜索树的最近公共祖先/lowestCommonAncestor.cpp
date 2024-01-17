dda127771180b127a090764b367dce8720e0582f
Zhenyu Dong <315300270@qq.com>
2024-01-17 16:39:01 +0800

    Solve LC.235

---
+++ Binary Tree/235. 二叉搜索树的最近公共祖先/lowestCommonAncestor.cpp
@@ -0,0 +1,30 @@
+// 递归
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root -> val > p -> val && root -> val > q -> val) {
+            return lowestCommonAncestor(root -> left, p, q);
+        } else if (root -> val < p -> val && root -> val < q -> val) {
+            return lowestCommonAncestor(root -> right, p, q);
+        } else {
+            return root;
+        }
+    }
+};
+
+
+
+// 迭代
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root) {
+            if (root->val > p->val && root->val > q->val) {
+                root = root->left;
+            } else if (root->val < p->val && root->val < q->val) {
+                root = root->right;
+            } else return root;
+        }
+        return NULL;
+    }
+};
