c253034f7760bbfc9abf832f912b0b73240ef666
Zhenyu Dong <315300270@qq.com>
2024-03-07 12:21:58 +0800

    Solve LC.322

---
+++ Dynamic Programming/322.零钱兑换/coinChange.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<unsigned int> dp(amount + 1, INT32_MAX);
+        dp[0] = 0;
+        for (int i = 0; i < coins.size(); i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+        return dp[amount] == INT32_MAX ? -1 : dp[amount];
+    }
+};
