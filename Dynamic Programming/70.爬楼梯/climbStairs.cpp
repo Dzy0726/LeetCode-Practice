a2e1929e44b38b62d1994f5ce0594ad4294e7202
Zhenyu Dong <315300270@qq.com>
2024-03-07 11:15:01 +0800

    Update Solution to LC.70

--- Dynamic Programming/70.爬楼梯/climbStairs.cpp
+++ Dynamic Programming/70.爬楼梯/climbStairs.cpp
@@ -20,3 +20,19 @@ public:
         return dp[n];
     }
 };
+
+// 变成背包问题呢？每次可以爬1-m个楼梯？
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> nums = {1, 2};
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int j = 1; j < n + 1; j++) {
+            for (int i = 0; i < nums.size(); i++) {
+                if (j >= nums[i]) dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[n];
+    }
+};
