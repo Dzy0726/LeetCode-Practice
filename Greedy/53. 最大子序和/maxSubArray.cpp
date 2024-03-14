5f824e11a0d8df70b92002dd95fee4728e1f7555
Zhenyu Dong <315300270@qq.com>
2024-03-14 14:24:17 +0800

    Update LC.53 DP method

--- Greedy/53. 最大子序和/maxSubArray.cpp
+++ Greedy/53. 最大子序和/maxSubArray.cpp
@@ -15,3 +15,16 @@ public:
         return result;
     }
 };
+
+// dp methods
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp = nums;
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i], dp[i - 1] + nums[i]);
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
