a5a2f6605c2a853e17d5fe56fad3ab9e0c66eab9
Zhenyu Dong <315300270@qq.com>
2024-03-14 10:18:05 +0800

    Solve LC.674

---
+++ Dynamic Programming/674.最长连续递增序列/findLengthOfLCIS.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
