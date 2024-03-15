161db70802827f17c8f7d5be0311d3a6f823cd21
Zhenyu Dong <dzyyy@connect.hku.hk>
2024-03-15 17:58:04 +0800

    Update LC.392 DP method

--- Dynamic Programming/392.判断子序列/isSubsequence.cpp
+++ Dynamic Programming/392.判断子序列/isSubsequence.cpp
@@ -22,3 +22,21 @@ public:
         else return false;
     }
 };
+
+// dp method
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        for (int i = 1; i < s.size() + 1; i++) {
+            for (int j = 1; j < t.size() + 1; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[s.size()][t.size()] == s.size();
+    }
+};
