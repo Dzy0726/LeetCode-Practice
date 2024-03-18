bb71463c98a3524196979e9da6bb0837457a1d71
Zhenyu Dong <315300270@qq.com>
2024-03-18 10:27:43 +0800

    Solve LC.516

---
+++ Dynamic Programming/516.最长回文子序列/longestPalindromeSubseq.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        vector<vector<int>> dp(s.size(), vector<int>(s.size(), 0));
+        for (int i = s.size() - 1; i >= 0; i--) {
+            for (int j = i; j < s.size(); j++) {
+                if (s[i] == s[j]) {
+                    if (j - i < 2) {
+                        dp[i][j] = j - i + 1;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    }
+                } else {
+                    dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][s.size() - 1];
+    }
+};
