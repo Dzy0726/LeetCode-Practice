04915a0dbf56229f89b70267879341185398dc66
Zhenyu Dong <dzyyy@connect.hku.hk>
2024-03-30 16:24:45 +0800

    12

--- test.cpp
+++ test.cpp
@@ -1,57 +1,35 @@
-/*** 
- * @Author: Zhenyu Dong
- * @Date: 2024-03-30 16:06:04
- * @LastEditors: Zhenyu Dong
- * @LastEditTime: 2024-03-30 16:06:13
- * @FilePath: \LeetCode-Practice\test.cpp
- * @Description: 
- * @
- * @Copyright (c) 2024 by Zhenyu Dong, All Rights Reserved. 
- */
-#include <cmath>
+#include <algorithm>
+#include <climits>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
-    int p, n, m;
-    cin >> p >> n >> m;
-
-    vector<int> monsters(n);
-    for (int i = 0; i < n; i++) {
-        cin >> monsters[i];
+    int N; 
+    cin >> N;
+    vector<int> aimLength;
+    while (N--) {
+        int x; 
+        cin >> x;
+        aimLength.push_back(x);
     }
-
-    for (int i = 0; i < m; i++) {
-        int boss_power;
-        cin >> boss_power;
-        int current_power = p;
-        int monster_count = 0;
-        int skill_used = 0;
-
-        if (current_power > boss_power) {
-            cout << 0 << endl;
-            continue;
-        }
-
-        for (int monster_power : monsters) {
-            while (current_power <= monster_power) {
-                current_power = floor(current_power * 1.1);
-                skill_used++;
-                if (current_power > boss_power) break;
+    int max_value = *max_element(aimLength.begin(), aimLength.end()) + 1;
+    vector<int> dp(max_value, INT_MAX);
+    dp[0] = 0;
+    dp[1] = 0;
+    for (int i = 1; i <= max_value; i++) {
+        for (int j = 1; j < i; j++) {
+            if (i % j == 0) {
+                int count = 2; // A+C
+                int pastes = (i / j) - 1;
+                count += pastes;
+                dp[i] = min(dp[i], dp[j] + count);
             }
-            current_power += monster_power;
-            monster_count++;
-            if (current_power > boss_power) break;
         }
-
-        while (current_power <= boss_power) {
-            current_power = floor(current_power * 1.1);
-            skill_used++;
-        }
-
-        cout << monster_count + skill_used << endl;
     }
+    for (int i = 0; i < aimLength.size(); i++) {
+        cout << dp[aimLength[i]] << endl;
+    }
+
     return 0;
 }
-// 64 位输出请用 printf("%lld")
